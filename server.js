18f03498c16f302e63b32bdb7042cead7336137a
Ali <65356558+aliUbaydullayev5@users.noreply.github.com>
2022-10-10 18:17:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const authRouter = require('./routers/auth.router')
 const productRouter = require('./routers/product.router')
+mongoose.connect('mongodb+srv://ali:python20050302@cluster0.1iyoajf.mongodb.net/?retryWrites=true&w=majority')
 
 const PORT = process.env.PORT || 8080
 const app = express()
@@ -17,13 +18,5 @@ app.get('/', (req, res)=> {
     res.send('<h1>Run Test</h1>')
 })
 
-function start() {
-    try{
-        mongoose.connect('mongodb+srv://ali:python20050302@cluster0.1iyoajf.mongodb.net/?retryWrites=true&w=majority')
-        app.listen(PORT, ()=> console.log(`Server has been started on PORT: ${PORT}`))
-    }catch (e) {
-        console.log(e)
-    }
-}
+app.listen(PORT, ()=> console.log(`Server has been started on PORT: ${PORT}`))
 
-start()
